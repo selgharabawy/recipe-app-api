735971931e7f9eb4bc9681cd3029352e58eaa9c2
selgharabawy <selgharabawy@outlook.com>
2023-12-04 09:48:18 +0200

    test recipe_image_file_path

--- app/recipe/tests/test_recipe_model.py
+++ app/recipe/tests/test_recipe_model.py
@@ -1,6 +1,7 @@
 """
 Test recipe models.
 """
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -24,3 +25,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('recipe.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None,'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
